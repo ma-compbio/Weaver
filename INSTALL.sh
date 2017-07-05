84d67b21d0054aacb88edaeab2e361c6bcc3dcd3
ashokrajaraman <ashokr@cs.cmu.edu>
2017-07-05 08:54:34 -0400

    Coverage calculations

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,6 +3,9 @@
 
 #sudo apt-get install libboost-all-dev
 
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BIN=$ROOT/bin
+export LD_LIBRARY_PATH=${BIN}/../Weaver_SV/lib:${LD_LIBRARY_PATH}
 cmake CMakeLists.txt
 
 make
